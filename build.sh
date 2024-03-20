d6a77f1b84bb01bd2c52b42c832bc1d200f5a8d2
PugzAreCute <me@pugzarecute.com>
2024-03-20 21:10:38 +0530

    ci: fix build script

--- build.sh
+++ build.sh
@@ -17,16 +17,16 @@ KVERSION=$(cat Makefile | grep -Pe "VERSION|LEVEL" | head -3 | awk '{print $3}' 
 
 LMK_TEST=$(cat arch/arm64/configs/vendor/spes-perf_defconfig | grep CONFIG_ANDROID_SIMPLE_LMK -q) # 0=SLMK, 1=CLO LMK
 
-if [ $1 == "MIUI"]; 
+if [ $1 == "MIUI "]; 
 then
-  if $LMK_TEST;
+  if [ $LMK_TEST ];
   then
   echo "Reverting LMK"
   git revert 379824bb737dd658bc69cd8edb773eb3405c77a7..1ab230774f638f0fa732bed4a005493638e15cb8
   BUILD_SUFFIX="${BUILD_SUFFIX}-MIUI"
   fi
 else
-  if $LMK_TEST;
+  if [ $LMK_TEST ];
   then
   echo "BUILD CRITICAL FAIL! LMK REVERT ON AOSP!"
   exit -1
@@ -63,7 +63,7 @@ else
   git pull
   cd ..
 fi
-TOOLCHAIN_PATHS="aosp_clang/bin/"
+TOOLCHAIN_PATHS="$(pwd)/aosp_clang/bin/"
 
 export PATH=${TOOLCHAIN_PATHS}:${PATH}
 
